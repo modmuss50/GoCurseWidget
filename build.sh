f85e67517808348bde6487cefa5bd1a066741fbd
modmuss50 <modmuss50@gmail.com>
2018-11-19 19:22:12 +0000

    Add version

--- build.sh
+++ build.sh
@@ -8,12 +8,18 @@ mkdir output
 
 platforms=("windows/amd64" "linux/amd64" "darwin/amd64")
 
+version="1.0.0"
+
+if ! [[ -z "${BUILD_NUMBER}" ]]; then
+  version=$version'.'${BUILD_NUMBER}
+fi
+
 for platform in "${platforms[@]}"
 do
     platform_split=(${platform//\// })
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
-    output_name='output/GoCurseWidget-'$GOOS'-'$GOARCH
+    output_name='output/GoCurseWidget-'$version'-'$GOOS'-'$GOARCH
     if [ $GOOS = "windows" ]; then
         output_name+='.exe'
     fi
